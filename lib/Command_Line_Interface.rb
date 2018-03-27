b43765560f74800ae6395080e09ca52a5218d2df
JMoloughney <github email address>
2018-03-27 17:17:11 -0400

    Added more functions to commandline

--- lib/Command_Line_Interface.rb
+++ lib/Command_Line_Interface.rb
@@ -30,27 +30,71 @@ def least_dangerous_borough
 end
 
 
-def crime_ids
-	Crime.all.map do |crime|
-		crime.id
-	end
-end
+# def crime_ids
+# 	Crime.all.map do |crime|
+# 		crime.id
+# 	end
+# end
 
-def location_ids
-	Location.all.map do |location|
-		location.id
-	end
-end
+# def location_ids
+# 	Location.all.map do |location|
+# 		location.id
+# 	end
+# end
 
  def type_of_crimes_borough(name)
- 	typ = {}
+ 	typ = Hash.new(0)
  	get_borough(name).each do |loc|
  		loc.crimes.each do |cr|
- 			typ[cr.offense] =+ 1
+ 			typ[cr.offense] += 1
  		end
  	end
  	typ
  end
+
+
+ def freq_crime_spots(name)
+ 	typ = Hash.new(0)
+ 	get_borough(name).each do |loc|
+ 		typ[loc.scene_of_crime] +=1
+ 	end
+ 	typ
+ end
+
+def freq_crime_level(name)
+	typ = Hash.new(0)
+ 	get_borough(name).each do |loc|
+ 		loc.crimes.each do |cr|
+ 			typ[cr.severity] += 1
+ 		end
+ 	end
+ 	typ
+ end
+
+
+ def freq_crime_by_month(name)
+ 	typ = Hash.new(0)
+ 	get_borough(name).each do |loc|
+ 		loc.crimes.each do |cr|
+ 			typ[cr.date_of_crime.split("-")[1]] +=1 
+ 		end
+ 	end
+ 	typ
+ end
+
+ def freq_crime_spots_by_month(name)
+ 	typ = {}
+ 	typ2 = Hash.new(0)
+ 	get_borough(name).each do |loc|
+ 		loc.crimes.each do |cr|
+ 			typ[cr.date_of_crime.split("-")[1]] = [typ2[cr.offense] +=1]
+ 		end
+ 	end
+ 	typ
+ end
+
+
+
 
 
 # def crime_by_location
